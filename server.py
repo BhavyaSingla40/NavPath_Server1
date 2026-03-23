492a3fd680f692ee4ac1970ff6f2a775a4c3a233
DivyanshGoel15 <72073034+DivyanshGoel15@users.noreply.github.com>
2026-03-24 02:39:03 +0530

    Enable unsafe Werkzeug in socketio.run

--- server.py
+++ server.py
@@ -679,4 +679,4 @@ if __name__ == '__main__':
 
     port = int(os.environ.get("PORT", 5000))
     log.info(f"Starting server on port {port}")
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
